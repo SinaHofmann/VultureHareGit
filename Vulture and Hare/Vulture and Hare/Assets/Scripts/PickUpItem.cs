d2cd84564b6347fc821e86101487812224538b8d
SinaHofmann <57066527+SinaHofmann@users.noreply.github.com>
2019-11-24 19:58:58 +0100

    Pick up Items

---
+++ Vulture and Hare/Vulture and Hare/Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpItem : MonoBehaviour
+{
+    
+    
+    private bool pickUpAllowed;
+
+
+    private void Update () {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            PickUp();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player")) 
+        {
+           
+            pickUpAllowed = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+          
+            pickUpAllowed = false;
+        }
+    }
+
+    void PickUp()
+    {
+        Destroy(gameObject);
+    }
+}
