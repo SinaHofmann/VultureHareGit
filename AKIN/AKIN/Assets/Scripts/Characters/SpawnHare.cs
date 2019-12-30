74e0d9e06eb36f3596bebf738244dba40107a8a0
SinaHofmann <57066527+SinaHofmann@users.noreply.github.com>
2019-12-30 20:59:37 +0100

    Started Dialogue System

--- AKIN/AKIN/Assets/Scripts/Characters/SpawnHare.cs
+++ AKIN/AKIN/Assets/Scripts/Characters/SpawnHare.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnHare : MonoBehaviour
 {
      public GameObject Hare;
 
+
    void OnTriggerEnter2D(Collider2D collision)
    {
+      
       if (collision.gameObject.name.Equals("Player"))
       {
            Hare.gameObject.SetActive(true);
+          
+           SceneManager.LoadScene(3);
            
       }
    }
-}
+   
+}  
+
+
+
+
